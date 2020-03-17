b69777bc2f82923f096d6e0de2448deeb976c66a
chetanrs <chetanrs@live.unc.edu>
2020-03-17 12:04:59 -0400

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+/*eslint-env browser*/
+
+var currency = 0;
+
+function myFunction() {
+    var counter = document.getElementById('root');
+    
+    counter.innerHTML = parseInt(counter.innerHTML) + 1;
+}
